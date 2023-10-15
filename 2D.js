d0500d3aa21faa519803e2fb0589d7c3cd9793dc
zahex360 <zahhod.sami@gmail.com>
2023-10-15 18:06:12 +0300

    JS_3-exercise_2

---
+++ 2D.js
@@ -0,0 +1,43 @@
+      const signatureCanvas = document.getElementById('signatureCanvas');
+      const clearSignatureButton = document.getElementById('clearSignature');
+      const signatureInput = document.getElementById('signatureInput');
+      const hireForm = document.getElementById('hireForm');
+
+      const context = signatureCanvas.getContext('2d');
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
+
+      let isDrawing = false;
+      let lastX = 0;
+      let lastY = 0;
+
+      signatureCanvas.addEventListener('mousedown', (e) => {
+        isDrawing = true;
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+      });
+
+      signatureCanvas.addEventListener('mousemove', draw);
+      signatureCanvas.addEventListener('mouseup', () => (isDrawing = false));
+      signatureCanvas.addEventListener('mouseout', () => (isDrawing = false));
+
+      clearSignatureButton.addEventListener('click', () => {
+        context.clearRect(0, 0, signatureCanvas.width, signatureCanvas.height);
+        signatureInput.value = ''; // Clear the hidden input value
+      });
+
+      function draw(e) {
+        if (!isDrawing) return;
+        context.beginPath();
+        context.moveTo(lastX, lastY);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.stroke();
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+        signatureInput.value = signatureCanvas.toDataURL(); // Save the image data in the hidden input
+      }
+
+      hireForm.addEventListener('submit', (e) => {
+        if (!signatureInput.value) {
+          alert('Please provide your signature.');
+          e.preventDefault(); // Prevent form submission if signature is missing
+        }
+      });
