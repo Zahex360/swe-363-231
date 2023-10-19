a8fbdb4052f9c43f30073973ff1d645048e0fef3
zahex360 <zahhod.sami@gmail.com>
2023-10-20 01:44:07 +0300

    JS_4-exercise_2

--- keyboard_nav.js
+++ keyboard_nav.js
@@ -1,30 +1,19 @@
+const navLinks = document.querySelectorAll("a");
 
-      // Add keyboard navigation to the navigation links
-      const navLinks = document.querySelectorAll("a");
+navLinks.forEach((link) => {
+  link.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" || event.key === " ") link.click();
+  });
+});
 
-      navLinks.forEach((link) => {
-        link.addEventListener("keydown", (event) => {
-          if (event.key === "Enter" || event.key === " ") {
-            link.click();
-          }
-        });
-      });
-      document.addEventListener("DOMContentLoaded", function () {
-        const helpPopup = document.getElementById("help-popup");
-      
-        // Check if it's not a mobile device
-        if (!isMobileDevice()) {
-          // Show the popup
-          helpPopup.style.display = "block";
-      
-          // Hide the popup after 5 seconds
-          setTimeout(function () {
-            helpPopup.style.display = "none";
-          }, 5000);
-        }
-      });
-      
-      function isMobileDevice() {
-        return window.innerWidth <= 768; 
-      }
-      
+document.addEventListener("DOMContentLoaded", () => {
+  const helpPopup = document.getElementById("help-popup");
+  if (!(() => window.innerWidth <= 768)()) {
+    (() => {
+      helpPopup.style.display = "block";
+      setTimeout(() => {
+        helpPopup.style.display = "none";
+      }, 5000);
+    })();
+  }
+});
