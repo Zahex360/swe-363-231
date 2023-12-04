abbd68954070765e2bfd9ec5ef0e491df93f965b
Zahex360 <zahhod.sami@gmail.com>
2023-12-04 14:27:04 +0300

    last version

--- BE3/hireMeMiddleware.js
+++ BE3/hireMeMiddleware.js
@@ -1,7 +1,10 @@
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const processHireForm = (req, res, next) => {
-  res.send('Form submitted successfully!');
+  // res.sendFile(path.join(__dirname, '../thank_you.html'));
+  res.send("Thank you! We have processed you form submission!")
 };
 
 module.exports = processHireForm;
